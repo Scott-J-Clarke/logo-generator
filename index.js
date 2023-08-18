7775b70a71b043327c0af0d43733548ef9e27c2f
Vivid Quill <sjdoublec@gmail.com>
2023-08-18 11:12:37 -0500

    Able to generate a 'logo.svg' but need to get data into it.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const inquirer = require('inquirer');
 const { Triangle, Square, Circle } = require("./lib/shapes.js"); // Desconstructed syntax
-// Use "require('fs')"" because writeFile is needed to create "logo.svg"?
+// Need "require('fs')" to use writeToFile. writeToFile will create "logo.svg":
 const fs = require('fs');
-
-// Depending on what shape we are importing, we will create an object from that class.
+const generateLogo = require('./lib/generateLogo.js'); // Brings in 'generateLogo()' function from 'generateLogo.js':
 
 const questions = [
     // It seems like "shape" should be the last question because it has the 'render()' function.
@@ -30,15 +29,13 @@ const questions = [
     }
 ]
 
-// Based on Module 9 Challenge, 'writeToFile()' function and 'init()' function work together. 
-
-// writeToFile function take its call from inside the 'init()' function:
+// writeToFile function is invoked from inside the 'init()' function:
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (error) => {
         if (error) {
             return console.log(error);
         }
-        console.log('Generated logo.svg'); // Changed string output for Module 10 Challenge.
+        console.log('Generated logo.svg'); // String output tells user in terminal that log has been made for them.
     }); 
 }
 
@@ -49,24 +46,25 @@ async function init() {
         switch (userResponses.shape) {
             case "triangle":
               shape = new Triangle();
-              console.log(shape.render()); // Testing to see if triangle points are returned.
-            //   console.log(shape.getText("abc")); // Testing to see if getText console.logs text.
+              console.log(shape.render()); // Test to check if triangle points are returned.
+            //   console.log(shape.getText("abc")); // Test to see if it returns 'abc' but it is 'undefined.'
               break;
             case "square":
               shape = new Square();
-              console.log(shape.render()); // Testing to see if square points are returned.
+              console.log(shape.render()); // Test to check if square points are returned.
               break;
             case "circle":
               shape = new Circle();
-              console.log(shape.render()); // Testing to see if circle points are returned.
+              console.log(shape.render()); // Test to check if circle points are returned.
               break;
           } 
         
-        // This writeToFile function invocation creates the logo.svg and puts it in 'examples' folder:
-        writeToFile('./examples/logo.svg', generateLogo(userResponses)); // 'generateLogo' function in separte file.
+        // writeToFile function creates 'logo.svg' and puts it in 'examples' folder:
+        writeToFile('./examples/logo.svg', generateLogo(userResponses)); 
 
         // console.log(userResponses); // Test to see what user input ('userResponses').
-        // Is there a way for the user tor review their logo choices (shape, text, textColor, shapeColor) before generating the logo? 
+        
+        // Could user review their logo choices (shape, text, textColor, shapeColor) before generating the logo? 
     } catch (error) {
         console.log(error);
     }
@@ -75,4 +73,3 @@ async function init() {
 init();
 
 // Will need to create a template with a particular SVG string.
-// This is similar to Module 9 Challenge where we created the README file.
